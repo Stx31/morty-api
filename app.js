5187e71ec55df38baf49b4f139f4bf9390a23972
Stx31 <kh1327303@gmail.com>
2024-01-25 14:54:46 +0100

    species

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function obtenerInformacionPersonaje() {
+function obtenerInformacionPersonajePorId() {
     var idPersonaje = document.getElementById('characterId').value;
     fetch(`https://rickandmortyapi.com/api/character/${idPersonaje}`)
         .then(response => response.json())
@@ -7,6 +7,7 @@ function obtenerInformacionPersonaje() {
             informacionPersonajeDiv.innerHTML = `
                 <h2>${data.name}</h2>
                 <p>Género: ${data.gender}</p>
+                <p>Especie: ${data.species}</p>
                 <p>Ubicación: ${data.location.name}</p>
                 <p>Estado: ${data.status}</p>
                 <p>Origen: ${data.origin.name}</p>
@@ -20,7 +21,36 @@ function obtenerInformacionPersonaje() {
         });
 }
 
-function filtrarPersonajes(genero) {
+function filtrarPersonajesPorEspecie(species) {
+    fetch(`https://rickandmortyapi.com/api/character/?species=${species}`)
+        .then(response => response.json())
+        .then(data => {
+            var informacionPersonajeDiv = document.getElementById('informacionPersonaje');
+            if (data.results.length > 0) {
+                var personajesHTML = data.results.map(personaje => `
+                    <div>
+                        <h2>${personaje.name}</h2>
+                        <p>Género: ${personaje.gender}</p>
+                        <p>Especie: ${personaje.species}</p>
+                        <p>Ubicación: ${personaje.location.name}</p>
+                        <p>Estado: ${personaje.status}</p>
+                        <p>Origen: ${personaje.origin.name}</p>
+                        <img src="${personaje.image}" alt="Foto de ${personaje.name}">
+                    </div>
+                `).join('');
+                informacionPersonajeDiv.innerHTML = personajesHTML;
+            } else {
+                informacionPersonajeDiv.innerHTML = '<p>No se encontraron personajes para la especie seleccionada.</p>';
+            }
+        })
+        .catch(error => {
+            console.error('Error al obtener los datos:', error);
+            var informacionPersonajeDiv = document.getElementById('informacionPersonaje');
+            informacionPersonajeDiv.innerHTML = '<p>Error al obtener los datos. Por favor, inténtalo de nuevo.</p>';
+        });
+}
+
+function filtrarPersonajesPorGenero(genero) {
     fetch(`https://rickandmortyapi.com/api/character/?gender=${genero}`)
         .then(response => response.json())
         .then(data => {
@@ -30,6 +60,7 @@ function filtrarPersonajes(genero) {
                     <div>
                         <h2>${personaje.name}</h2>
                         <p>Género: ${personaje.gender}</p>
+                        <p>Especie: ${personaje.species}</p>
                         <p>Ubicación: ${personaje.location.name}</p>
                         <p>Estado: ${personaje.status}</p>
                         <p>Origen: ${personaje.origin.name}</p>
@@ -58,6 +89,7 @@ function mostrarTodosLosPersonajes() {
                     <div>
                         <h2>${personaje.name}</h2>
                         <p>Género: ${personaje.gender}</p>
+                        <p>Especie: ${personaje.species}</p>
                         <p>Ubicación: ${personaje.location.name}</p>
                         <p>Estado: ${personaje.status}</p>
                         <p>Origen: ${personaje.origin.name}</p>
